3716a556ef20d91956f8815483a58f2e7a704cd9
Yeshwanth Shankar <nithyanandan1753@gmail.com>
2024-11-27 22:19:06 +0530

    first commit

---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const MyComponent = () => {
+    return (
+      <div>
+        <h1>Hello this my First React Component</h1>
+      </div>
+    );
+  };
+export default MyComponent;
